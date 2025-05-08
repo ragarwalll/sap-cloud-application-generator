9a0764ff232ecb766f064411eb7287d4111cd5ac
ragarwalll <agarwal.rahul324@gmail.com>
2025-05-08 22:43:20 +0530

    feat(breaking): upgrade springboot project template SpringBoot -> 3.4.5 XSUAASecurity -> 3.6.0

--- templates/base/backend/springboot/src/main/java/{{projectResourceRoots}}/{{deleteIfNotEmpty securityConfiguration}}.java
+++ templates/base/backend/springboot/src/main/java/{{projectResourceRoots}}/{{deleteIfNotEmpty securityConfiguration}}.java
@@ -1,44 +1,41 @@
 package {{projectNamespace}}.{{projectName}};
 
-import com.sap.cloud.security.xsuaa.XsuaaServiceConfiguration;
-import com.sap.cloud.security.xsuaa.token.TokenAuthenticationConverter;
+import com.sap.cloud.security.spring.config.IdentityServicesPropertySourceFactory;
+import com.sap.cloud.security.spring.config.XsuaaServiceConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@PropertySource(factory = IdentityServicesPropertySourceFactory.class, ignoreResourceNotFound = true, value = { "" }) // might be auto-configured in a future release
+public class SecurityConfiguration {
 
     @Autowired
     XsuaaServiceConfiguration xsuaaServiceConfiguration;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        // @formatter:off
+    @Autowired
+    Converter<Jwt, AbstractAuthenticationToken> authConverter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeRequests()
-                .antMatchers("/health").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .oauth2ResourceServer()
-                .jwt()
-                .jwtAuthenticationConverter(getJwtAuthoritiesConverter());
-        // @formatter:on
-    }
+            .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/health").permitAll()
+                    .anyRequest().authenticated()
+            )
+            .oauth2ResourceServer(oauth2 -> oauth2
+                    .jwt(jwt -> jwt.jwtAuthenticationConverter(authConverter)));
 
-    Converter<Jwt, AbstractAuthenticationToken> getJwtAuthoritiesConverter() {
-        TokenAuthenticationConverter converter = new TokenAuthenticationConverter(xsuaaServiceConfiguration);
-        converter.setLocalScopeAsAuthorities(true); // not applicable in case of multiple xsuaa bindings!
-        return converter;
+        return http.build();
     }
-
 }
